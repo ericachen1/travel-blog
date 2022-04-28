91ae753a457108d896a6eb993cf1a9e2f92fb281
erica chen <ericachen70819@gmail.com>
2022-04-28 14:38:37 -0400

    GENESIS2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>Travel Blog</h1>
-            <p>Explore the world with us</p>
+            <h1>Blog de Arte Español en Nueva York | Erica Chen</h1>
+            <h3>Audioguías de arte por ... Domingo Zapata, J Demsky y Manuel Chili </h3>
         </div>
     )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,9 +3,21 @@ import React from 'react'
 const Post = (props) => {
     return (
         <div className="post">
-            <h2> {props.name} </h2>
-            <img src={props.image} alt="blog location"></img>
-            <p> {props.text} </p>
+            <h1></h1>
+            <img src={props.image} alt="ART"></img> 
+            <h2> {props.title} </h2>
+            <p> {props.artist} </p>
+            <h3> {props.name} </h3>
+            <p> {props.year} </p>
+            <p> {props.style} </p>
+            <p> {props.type} </p>
+            
+            <p> {props.justification} </p>
+            <p> {props.contexto} </p>
+            <p> {props.conexion} </p>
+
+            <iframe width="420" height="315" src={props.text} ></iframe>
+
         </div> 
     )
 }
